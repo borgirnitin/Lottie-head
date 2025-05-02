bd43cbb34c204e20dfe8eca82abcdf7bba96bd0a
borgirnitin <140273213+borgirnitin@users.noreply.github.com>
2025-05-01 19:04:54 -0700

    Update LottieViewer.js

--- components/LottieViewer.js
+++ components/LottieViewer.js
@@ -33,7 +33,7 @@ export default function LottieViewer() {
         width: "100vw",
         height: "100vh",
         overflow: "hidden",        
-        backgroundColor: "transparent"
+        backgroundColor: "1A1A1A"
       }}
     />
   )
